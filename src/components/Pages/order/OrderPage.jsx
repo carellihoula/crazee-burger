481e6609a1dde61dbe58c21d146236eae62f133b
carellihoula <carellihoula@gmail.com>
2023-07-30 15:24:43 +0200

    F02: OrderPAge> modify

--- src/components/Pages/order/OrderPage.jsx
+++ src/components/Pages/order/OrderPage.jsx
@@ -1,24 +1,16 @@
 //import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import LoginPage from '../login/LoginPage';
-function OrderPage() {
 
-    const handleClickDeconnect = () =>{
-            var a = 10;
-            alert(a)
-    }
+function OrderPage() {
 
   return (
 
     <div>
             <h1>Bonjour {}</h1>
             <Link to="/">
-               <button onClick={handleClickDeconnect}>Deconnexion</button>
-            </Link>
-            <Routes>
-                <Route path="rr" element={<OrderPage/>} />
-            </Routes>
-            
+               <button>Deconnexion</button>
+            </Link>     
     </div>
   )
 }
