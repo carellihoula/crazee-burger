418b698f94e7dec8b99b7fda2b8a9d4ea89e6782
carellihoula <carellihoula@gmail.com>
2023-08-01 23:38:16 +0200

    main : OrderPage

--- src/components/Pages/order/OrderPage.jsx
+++ src/components/Pages/order/OrderPage.jsx
@@ -1,6 +1,25 @@
 //import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 //import LoginPage from '../login/LoginPage';
+import styled from 'styled-components';
+import { theme } from '../../../theme';
+
+const Container = styled.div`
+    display : flex;
+    justify-content: center;
+    align-items: center;
+    background: ${theme.colors.primary};
+    width:100%;
+    height:100vh;
+    .sous-container{
+    
+    width: 95%;
+    height: 94%;
+    border-radius: 15px;
+    background: #FFF;
+    }
+`;
+
 
 
 function OrderPage() {
@@ -11,13 +30,16 @@ function OrderPage() {
 
   //Output
   return (
-
-    <div>
-            <h1>Bonjour {username.toLowerCase()}</h1>
+          
+    <Container>
+      <div className="sous-container">
+            {/*<h1>Bonjour {username.toLowerCase()}</h1>
             <Link to="/">
                <button>Deconnexion</button>
-            </Link>     
-    </div>
+            </Link> */}
+      </div>
+             
+    </Container>
   )
 }
 
